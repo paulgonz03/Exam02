a9c2b9d0674a0bfe77a88789fa1c4a30e718bbd8
Paula <paula.gonser@icloud.com>
2025-05-19 21:27:13 +0200

    mas cambios

--- 3/rstr_capitalizer.c
+++ 3/rstr_capitalizer.c
@@ -1,36 +1,30 @@
 #include <unistd.h>
 
-void	rstr_capitalizer(char *str)
+void capitalizer(char *str)
 {
-	int		i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] >= 'A' && 'Z' >= str[i])
-			str[i] += 32;
-		if ((str[i] >= 'a' && 'z' <= str[i]) && (str[i + 1] == ' ' || \
-			str[i + 1] == '\t' || str[i + 1] == '\0'))
-			str[i] -= 32;
-		write(1, &str[i++], 1);
-	}
+    int i = 0;
+    while(str[i])
+    {
+        if(str[i] >= 'A' && str[i] <= 'Z')
+            str[i] += 32;
+        if((str[i] >= 'a' && str[i] <= 'z') && (str[i+1] == ' ' || str[i+1] == '\t' || str[i+1] == '\0'))
+            str[i] -= 32;
+        write(1, &str[i++], 1);
+    }
 }
-
-
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-	int i ;
-
-	if (argc < 1)
-		write(1, "\n", 1);
-	else 
-	{
-		i = 1;
-		while (i < argc)
-		{
-			rstr_capitalizer(argv[i]);
-			write(1, "\n", 1);
-			i++;	
-		}
-	}
+    int i = 1;
+    if(ac > 1)
+    {
+        while(i < ac)
+        {
+            capitalizer(av[i]);
+            write(1, "\n", 1);
+            i++;
+        }
+    }
+    else
+        write(1, "\n", 1);
+    return(0);
 }
